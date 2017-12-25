27fb0db604a071772ec783d5b660296dfe1f13b9
Ilikepotato <33576713+Ilikepotato@users.noreply.github.com>
2017-12-25 09:39:56 +0800

    例2-2 在屏幕上显示两个短句

---
+++ Examples/2-2.c
@@ -0,0 +1,15 @@
+/*例2-2
+在屏幕上显示两个短句"Programming is fun."和"And programming in C
+is even more fun!"，每行显示一句。*/
+
+/*显示两行文字*/
+
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+	printf("Programming is fun.\n");
+	printf("And programming in C is even more fun!\n");
+
+	return 0;
+}
