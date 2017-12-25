accf17591ae50373291ae3f68597cb5c8582ce45
Ilikepotato <33576713+Ilikepotato@users.noreply.github.com>
2017-12-25 10:06:27 +0800

    练习2-3 输出倒三角图案

---
+++ Exercises/2-3.c
@@ -0,0 +1,20 @@
+/*练习2-3
+输出倒三角图案：
+在屏幕上显示如下倒三角图案。
+	* * * *
+	 * * *
+	  * *
+	   *
+*/
+
+#include <stdio.h>							//编译预处理命令
+
+int main(int argc, char const *argv[])		//定义主函数main()
+{
+	printf("	* * * *\n");
+	printf("	 * * *\n");
+	printf("	  * *\n");
+	printf("	   *\n");
+
+	return 0;								//返回一个整数
+}
