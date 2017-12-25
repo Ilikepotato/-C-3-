49eaa0a10f64a0d029f55bc1f1c5905f8bae12ca
Ilikepotato <33576713+Ilikepotato@users.noreply.github.com>
2017-12-25 09:37:22 +0800

    例1-1 求阶乘问题。

--- Examples/1-1.c
+++ Examples/1-1.c
@@ -1,17 +1,17 @@
 /*例1-1 求阶乘问题。
 输入一个正整数n，输出n！*/
 
-#include <stdio.h>						//编译预处理命令
+#include <stdio.h>					//编译预处理命令
 
 int factorial(int n);					//函数声明
 
 int main(int argc, char const *argv[])	//主函数
 {
-	int n;								//变量定义
+	int n;						//变量定义
 
-	//scanf("%d", &n);					//输入一个整数
+	//scanf("%d", &n);				//输入一个整数
 	n = 5;
-	printf("%d\n", factorial(n));		//调用函数计算阶乘
+	printf("%d\n", factorial(n));			//调用函数计算阶乘
 
 	return 0;
 }
